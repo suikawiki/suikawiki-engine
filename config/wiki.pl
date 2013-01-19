79f6f7d6106178406f581e531d42a1d809f6932f
Wakaba <w@suika.fam.cx>
2013-01-19 13:59:29 +0900

    domain

--- config/wiki.pl
+++ config/wiki.pl
@@ -9,8 +9,8 @@ our $homepage_name = 'HomePage';
 our $license_name = 'WikiPageLicense';
 our $help_page_name = 'HelpPage';
 
-our $style_url = q<http://suika.fam.cx/swe/styles/sw>;
-our $script_url = q<http://suika.fam.cx/swe/scripts/sw>;
+our $style_url = q</swe/styles/sw>;
+our $script_url = q</swe/scripts/sw>;
 our $cvs_archives_url = q</gate/cvs/melon/pub/suikawiki/sw4data/>;
 
 our $sw3_db_dir_name = q[/home/wakaba/server/sw3/wikidata/page/];
