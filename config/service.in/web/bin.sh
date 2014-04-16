2bdd64932b1cc5d43f73cea6e8b3061e0648336a
Wakaba <wakaba@suikawiki.org>
2014-04-16 18:53:01 +0900

    path

--- config/service.in/web/bin.sh
+++ config/service.in/web/bin.sh
@@ -5,8 +5,8 @@ export TZ=UTC
 export SW_DB_DIR=@@ROOT@@/data
 export KARASUMA_CONFIG_JSON=@@ROOT@@/config/@@ENV@@.json
 export KARASUMA_CONFIG_FILE_DIR_NAME=@@ROOT@@/local/keys/@@ENV@@
-export WEBUA_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/SWE/modules/karasuma-config/bin/get-json-config.pl env_WEBUA_DEBUG text`
-port=`@@ROOT@@/perl @@ROOT@@/modules/SWE/modules/karasuma-config/bin/get-json-config.pl web_port text`
+export WEBUA_DEBUG=`@@ROOT@@/modules/SWE/perl @@ROOT@@/modules/SWE/modules/karasuma-config/bin/get-json-config.pl env_WEBUA_DEBUG text`
+port=`@@ROOT@@/modules/SWE/perl @@ROOT@@/modules/SWE/modules/karasuma-config/bin/get-json-config.pl web_port text`
 
 eval "exec setuidgid @@USER@@ @@ROOT@@/modules/SWE/plackup \
     $PLACK_COMMAND_LINE_ARGS \
