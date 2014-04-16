4e8ff909c15f9866e1a176b015063677a5251819
Wakaba <wakaba@suikawiki.org>
2014-04-16 17:11:18 +0900

    deploy

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
-# -*- Makefile -*-
-
 all:
 
 WGET = wget
@@ -14,30 +12,50 @@ updatenightly: local/bin/pmbp.pl
 
 ## ------ Setup ------
 
-deps: git-submodules pmbp-install
+deps: git-submodules
+	cd modules/SWE && $(MAKE) deps
 
 git-submodules:
 	$(GIT) submodule update --init
 
-local/bin/pmbp.pl:
-	mkdir -p local/bin
-	$(WGET) -O $@ https://raw.github.com/wakaba/perl-setupenv/master/bin/pmbp.pl
-pmbp-upgrade: local/bin/pmbp.pl
-	perl local/bin/pmbp.pl --update-pmbp-pl
-pmbp-update: git-submodules pmbp-upgrade
-	perl local/bin/pmbp.pl --update
-pmbp-install: pmbp-upgrade
-	perl local/bin/pmbp.pl --install \
-            --create-perl-command-shortcut perl \
-            --create-perl-command-shortcut prove
+## ------ Server configuration ------
 
-## ------ Tests ------
+# Need SERVER_ENV!
+server-config: daemontools-config
 
-PROVE = ./prove
+# Need SERVER_ENV!
+install-server-config: install-daemontools-config
 
-test: test-deps test-main
+SERVER_ENV = HOGE
+SERVER_ARGS = \
+    APP_NAME=suikawiki \
+    SERVER_INSTANCE_NAME="suikawiki-$(SERVER_ENV)" \
+    SERVER_INSTANCE_CONFIG_DIR="$(abspath ./config)" \
+    ROOT_DIR="$(abspath .)" \
+    LOCAL_DIR="$(abspath ./local)" \
+    LOG_DIR=/var/log/app \
+    SYSCONFIG="/etc/sysconfig/suikawiki" \
+    SERVICE_DIR="/service" \
+    SERVER_USER=wakaba LOG_USER_GROUP=wakaba.wakaba \
+    SERVER_ENV="$(SERVER_ENV)"
 
-test-deps: deps
+# Need SERVER_ENV!
+daemontools-config:
+	$(MAKE) --makefile=Makefile.service all $(SERVER_ARGS) SERVER_TYPE=web
 
-test-main:
-	$(PROVE) t/*.t
+# Need SERVER_ENV!
+install-daemontools-config:
+	mkdir -p /var/log/app
+	chown wakaba.wakaba /var/log/app
+	$(MAKE) --makefile=Makefile.service install $(SERVER_ARGS) SERVER_TYPE=web
+
+## ------ Deployment ------
+
+CINNAMON_GIT_REPOSITORY = git://github.com/wakaba/cinnamon.git
+
+cinnamon:
+	mkdir -p local
+	cd local && (($(GIT) clone $(CINNAMON_GIT_REPOSITORY)) || (cd cinnamon && $(GIT) pull)) && cd cinnamon && $(MAKE) deps
+	echo "#!/bin/sh" > ./cin
+	echo "exec $(abspath local/cinnamon/perl) $(abspath local/cinnamon/bin/cinnamon) \"\$$@\"" >> ./cin
+	chmod ugo+x ./cin
---
+++ config/deploy.pl
@@ -0,0 +1,99 @@
+use strict;
+use warnings;
+use Cinnamon::DSL;
+use Cinnamon::Task::Git;
+use Cinnamon::Task::Daemontools;
+
+set application => 'suikawiki';
+set git_repository => 'http://suika.suikawiki.org/gate/git/bare/suikawiki-driver.git';
+set deploy_dir => '/home/wakaba/server/suikawiki';
+set daemontools_service_dir => '/service';
+set get_daemontools_service_name => sub {
+  return sub {
+    return sprintf '%s-%s-web', (get 'application'), (get 'server_env');
+  };
+};
+set get_daemontools_log_file_name => sub {
+  return sub {
+    sprintf '/var/log/app/%s-%s-web/current',
+        (get 'application'), (get 'server_env');
+  };
+};
+
+role L1 => 'iyokan', {
+  server_env => 'L1',
+};
+
+task deploy => sub {
+  my ($host, @args) = @_;
+  call 'update', $host, @args;
+  call 'setup', $host, @args;
+  call 'restart', $host, @args;
+};
+
+task deploy_full => sub {
+  my ($host, @args) = @_;
+  remote { sudo '' } $host;
+  call 'update', $host, @args;
+  call 'setup', $host, @args;
+  call 'install', $host, @args;
+  call 'restart', $host, @args;
+};
+
+task update => sub {
+  my ($host, @args) = @_;
+  call 'git:update', $host, @args;
+  call 'keys:update', $host, @args;
+};
+
+task setup => sub {
+  my ($host, @args) = @_;
+  call 'app:setup', $host, @args;
+};
+
+task install => sub {
+  my ($host, @args) = @_;
+  call 'app:install', $host, @args;
+};
+
+task restart => sub {
+  my ($host, @args) = @_;
+  call 'web:restart', $host, @args;
+};
+
+task keys => {
+  update => sub {
+    my ($host, @args) = @_;
+    my $dir = get 'deploy_dir';
+    my $env = get 'server_env';
+    remote {
+      run 'mkdir', '-p', "$dir/local/keys/$env";
+    } $host;
+    run 'rsync', '-av', "local/keys/$env" => "$host:$dir/local/keys/";
+  },
+};
+
+task app => {
+  setup => sub {
+    my ($host, @args) = @_;
+    my $dir = get 'deploy_dir';
+    my $name = get 'server_env';
+    remote {
+      run qq{cd \Q$dir\E && make deps server-config SERVER_ENV=$name};
+    } $host;
+  },
+  install => sub {
+    my ($host, @args) = @_;
+    my $dir = get 'deploy_dir';
+    my $name = get 'server_env';
+    remote {
+      sudo qq{sh -c "cd \Q$dir\E && LANG=C make install-server-config SERVER_ENV=$name"};
+    } $host;
+  },
+}; # app
+
+task web => {
+  (define_daemontools_tasks 'web'),
+};
+
+1;
